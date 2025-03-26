d873918c35b79c0c971839ea2b72f879f24e7933
Rohit Shrestha <shrestha.rohit655@gmail.com>
2025-03-26 20:09:57 +0545

    update : updared the landing page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={32} height={32} />
+          <h2>Prepme</h2>
+        </Link>
+      </nav>
+      {children}
+    </main>
+  );
 };
 
 export default RootLayout;
