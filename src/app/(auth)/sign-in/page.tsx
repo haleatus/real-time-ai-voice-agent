ad10b1b184778070ef7801321519edb32806bbf5
Rohit Shrestha <shrestha.rohit655@gmail.com>
2025-03-26 19:08:39 +0545

    update : completed the UI for auth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/base/auth/auth-form";
 import React from "react";
 
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default SigninPage;
