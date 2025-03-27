b17b07cefea89ae6c8a96a07779a31b9a93fa3cb
Rohit Shrestha <shrestha.rohit655@gmail.com>
2025-03-27 12:45:58 +0545

    update : added action for signin as well

--- src/lib/actions/auth.action.ts
+++ src/lib/actions/auth.action.ts
@@ -1,10 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
-import { db } from "@/firebase/admin";
+// Firebase imports
+import { auth, db } from "@/firebase/admin";
+
+// Next cookies
+import { cookies } from "next/headers";
+
+// One week
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 /**
- * Sign up parameters
+ * Sign up action
  * @param params
  * @returns {Promise} Promise object represents the result of the sign up
  */
@@ -47,4 +54,62 @@ export async function signUp(params: SignUpParams) {
       message: "Failed to create an account.",
     };
   }
+}
+
+/**
+ * Sign in action
+ * @param params
+ * @returns {Promise} Promise object represents the result of the sign in
+ */
+export async function signIn(params: SignInParams) {
+  // Destruct the parameters
+  const { email, idToken } = params;
+
+  try {
+    // Get access to the user
+    const userRecord = await auth.getUserByEmail(email);
+
+    // If the user does not exists, return an error message
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exists. Create and account instead.",
+      };
+    }
+
+    // If the user exists, set the session cookie
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    // Log the error
+    console.error("Error signing in a user", error);
+    // Return an error message
+    return {
+      success: false,
+      message: "Failed to log in to an account.",
+    };
+  }
+}
+
+/**
+ * setSessionCookie action
+ * @param params
+ * @returns
+ */
+export async function setSessionCookie(idToken: string) {
+  // store the cookie
+  const cookieStore = await cookies();
+
+  // create a session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000, // in milliseconds
+  });
+
+  // set the cookie
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
