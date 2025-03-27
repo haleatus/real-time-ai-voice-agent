3b1025e56aa405545738f9e7cd60a5e93cda05dc
Rohit Shrestha <shrestha.rohit655@gmail.com>
2025-03-27 16:24:35 +0545

    update : integrated to show the interviews in landing page

---
+++ src/lib/actions/interview.action.ts
@@ -0,0 +1,48 @@
+"use server";
+
+// Firebase imports
+import { db } from "@/firebase/admin";
+
+/**
+ * Interview interface
+ * @param userId
+ * @returns Interview list created by the user
+ */
+export async function getInterviewsByUserId(
+  userId: string
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+/**
+ * Interview interface
+ * @param userId
+ * @returns Latest Interview list created by the users
+ */
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
